950fd136ebc4c1508155b002c09db71fcdb0081f
Rohit Kumar <rohit.kumar@mail.vinove.com>
2019-07-05 19:04:19 +0530

    first commit

---
+++ FeedsDemo/FeedsDemo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FeedsDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public List<int> GetData = new List<int>();
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            for (var i = 0; i < 6; i++)
+            {
+                GetData.Add(1);
+            }
+            WorkoutList.ItemsSource = GetData;
+        }
+    }
+}
